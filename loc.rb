50191c98da3d42361f632cc78559c748717e5e2b
reshubhan <reshubhan@gmail.com>
2013-02-07 12:44:50 +0530

    Update loc.rb

--- loc.rb
+++ loc.rb
@@ -1,6 +1,6 @@
 # This is a testing file, for testing lines of code
 #for ticket OTWO-2492
-# this has got 4 blank lines , 4 code lines and 3 comments lines, so total code of line should be 11.
+# this has got 5 blank lines , 5 code lines and 3 comments lines, so total code of line should be 11.
 
 
 
