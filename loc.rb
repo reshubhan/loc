ba496a5d402b20c1866f7a65f2dcc7d6065c721e
Amar Singh <amar.k@imaginea.com>
2013-02-07 12:39:41 +0530

    First commit

---
+++ loc.rb
@@ -0,0 +1,12 @@
+# This is a testing file, for testing lines of code
+#for ticket OTWO-2492
+# this has got 4 blank lines , 4 code lines and 3 comments lines, so total code of line should be 11.
+
+
+
+
+If 2 > 3
+  puts "this is right"
+else
+  puts "this is wrong"
+end
